5175b4fe5315651fc61cd6639a46cbd39fe9dc55
Nishant Bhai <nishantbhai@Nishants-MacBook-Pro-2.local>
2016-06-30 02:40:11 +0530

    changed port no

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.get('/channelInfo/', function(req, res){
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
 });
